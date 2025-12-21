15bee3b2409c2e1eacb02009739ec1da90b7264a
AswinMThambi <aswinmthambi2@gmail.com>
2025-12-21 13:31:50 +0530

    Day 6

---
+++ lists.py
@@ -0,0 +1,18 @@
+numbers=[10,20,30,40,50,60,70]
+
+print ("All the numbers : ")
+for i in numbers :
+	print (i)
+
+print ("Numbers greater than 25 : ")
+for i in numbers :
+	if(i<25):
+		print (i)
+
+
+names=["aswin","tintu","thambi"]
+
+for name in names :
+	greet=f"Hello {name}"
+	print (greet)
+
