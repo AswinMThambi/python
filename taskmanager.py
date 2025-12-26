3d09060f3dd2a9f52f41703f3802a9ec614b5d96
AswinMThambi <aswinmthambi2@gmail.com>
2025-12-26 11:09:23 +0530

    Small modifications

--- taskmanager.py
+++ taskmanager.py
@@ -1,47 +1,50 @@
 tasks = []
 
 def add_task(title) :
-	task = {"title" : title,
-		"done" : False}
+	task = {"title" : title,"done" : False}
 	tasks.append(task)
-	print ("Task added successfully!")
+	print ("Task has been added successfully!\n")
 
-def view_tasks() : 
-	if not tasks : 
-		print ("There is no tasks...")
+def view_tasks() :
+	if not tasks :
+		print ("There are no tasks..\n")
 		return
 
-	for index, task in enumerate(tasks) :
+	print ("Here's the list of tasks-\n")
+	for index,task in enumerate(tasks) :
 		status = "Done" if task["done"] else "Pending"
-		print (f"{index + 1}.{task["title"]}-{status}")
+		print (f"{index + 1}.{task['title']}-{status}\n\n")
 
-def mark_done(task_number) :
-	if task_number < 1 or task_number > len(tasks) : 
-		print ("Not valid")
+def mark_as_done(task_number) :
+	if task_number < 1 or task_number > len(tasks) :
+		print ("Invalid Input..\n")
 		return
 
-	tasks[task_number-1]["done"] = True
-	print ("Task marked as done!")
+	tasks[task_number - 1]["done"] = True
+	print ("Task marked as done!\n")
 
 while True :
-	print ("1) Add Tasks\n")
+	print ("\nWelcome to the Task_Manager\n\nChoose your action : ")
+	print ()
+	print ("1)Add Task\n")
 	print ("2)View Tasks\n")
-	print ("3)Mark Tasks As Done\n")
+	print ("3)Mark Tasks Done\n")
 	print ("4)Exit\n")
 
-	choice = input("Enter your selection..\n")
-
+	choice = input()
 	if choice == "1" :
-		title = input("Task Title Please:\n")
+		title = input ("Enter the task-\n")
 		add_task(title)
 
 	elif choice == "2" :
 		view_tasks()
+
 	elif choice == "3" :
-		task_number = int(input("Give the completed task nuumber\n"))
-		mark_done(task_number)
+		task_number = int(input("Task Number Please -\n"))
+		mark_as_done(task_number)
+
 	elif choice == "4" :
 		print ("Exiting...\n")
 		break
-	else :
-		print ("Invalid Input!") 
+	else:
+		print ("Invalid Input\n")
